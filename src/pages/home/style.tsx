09f3e53c55c87884dd68daf60ac27f986e312f8a
steph-crown <emmanuelstephen024@gmail.com>
2022-01-06 20:11:13 +0100

    ...

--- src/pages/home/style.tsx
+++ src/pages/home/style.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import BgPattern from "./../../assets/background-vector-min.png";
 
 export const HomePageContainer = styled.div`
-    background: url(${BgPattern});
+    background: linear-gradient(rgba(30, 35, 41, 0.6), rgba(30, 35, 41, 0.6)),
+        url(${BgPattern});
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
